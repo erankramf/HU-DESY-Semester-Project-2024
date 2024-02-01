803a4813b424f10ccd3392e2765ff87817ec506c
Selin Orhanlar <140173805+selinorhanlar@users.noreply.github.com>
2024-02-01 14:32:19 +0100

    frontend query data function

--- frontend/src/api/Service.ts
+++ frontend/src/api/Service.ts
@@ -1,20 +1,26 @@
+import { teal } from '@mui/material/colors';
 import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000',
     timeout: 1000,
-  });
+});
 
 export const getTelescopes = () => {
     return instance.get('/Telescopes');
 }
-export const getParams = (telescopeName : string) =>{
+export const getParams = (telescopeName: string) => {
     return instance.get(`/Telescopes/${telescopeName}`);
 }
-export const getVersions = (telescopeName : string, parameterName : string) =>{
+export const getVersions = (telescopeName: string, parameterName: string) => {
     return instance.get(`/Telescopes/${telescopeName}/${parameterName}`);
 }
 
-export const getData = (telName : string, param : string, version : string) => {
-    return instance.get(`/Telescopes/${telName}/${param}/${version}`)
+// export const getData = (telName : string, param : string, version : string) => {
+//     return instance.get(`/Telescopes/${telName}/${param}/${version}`)
+// }
+
+//neue get_data Funktion für mehrere Versionen auf einmal
+export const getMoreData = (telName: string, param: string, versions: any[string]) => {
+    return instance.get(`/Telescopes/${telName}/${param}/${versions}`)
 }
