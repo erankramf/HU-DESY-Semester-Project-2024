78526906c74c9d3ee1f73e2d27b284aa24151641
bluestar04 <syeda.kun@gmail.com>
2024-02-04 16:53:55 +0100

    Did the table component

---
+++ frontend/src/components/Table.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
+const DataTable = (data:any) => {
+  // Assuming data is an array of objects with keys representing table columns
+  const columns = Object.keys(data[0]);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column}>{column}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row:any, index:any) => (
+            <TableRow key={index}>
+              {columns.map((column) => (
+                <TableCell key={column}>{row[column]}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default DataTable;
