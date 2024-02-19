2f5c2601fb288505df1fcc4d1c4a0cc4c4cb8770
Selin Orhanlar <140173805+selinorhanlar@users.noreply.github.com>
2024-02-19 13:09:01 +0100

    error handle

--- frontend/src/api/Service.ts
+++ frontend/src/api/Service.ts
@@ -3,18 +3,20 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000',
     timeout: 1000,
-  });
+});
 
 export const getTelescopes = () => {
     return instance.get('/Telescopes');
 }
-export const getParams = (telescopeName : string) =>{
+export const getParams = (telescopeName: string) => {
     return instance.get(`/Telescopes/${telescopeName}`);
 }
-export const getVersions = (telescopeName : string, parameterName : string) =>{
+export const getVersions = (telescopeName: string, parameterName: string) => {
     return instance.get(`/Telescopes/${telescopeName}/${parameterName}`);
 }
 
-export const getData = (telName : string, param : string, version : string) => {
-    return instance.get(`/Telescopes/${telName}/${param}/${version}`)
+export const getData = (telName: string, param: string, version: string) => {
+    if (version === null) {
+        return console.error("you have to pick a version");
+    } else return instance.get(`/Telescopes/${telName}/${param}/${version}`)
 }
