5dc78cb0db2063934e2c787e8f14ff9f71c4dc21
Selin Orhanlar <140173805+selinorhanlar@users.noreply.github.com>
2024-03-10 10:40:36 +0100

    sizing of MetaData

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,44 +6,45 @@ import { bgcolor } from '@mui/system'
 import DataTable from './components/Table'
 //South-LST-D
 const useStyles = createStyles({
-  blah:{
+  blah: {
   }
 })
 
-interface Datapoint{
+interface Datapoint {
   Parameter: string;
   Value: any;
 }
 
-interface VersionData{
-  Data: Datapoint[]; 
+interface VersionData {
+  Data: Datapoint[];
 }
 function App() {
   const [text, setText] = useState("");
   const [verData, setVerData] = useState<Datapoint[]>([]);
   return (
     <>
-    <Container sx={{height:'30vh', bgcolor:'background.paper'}}>
-      <DataSelector onGotData={(data) => {if (!data) return;
-      setText(JSON.stringify(data, null, 2));
-      var list : Datapoint[] = [];
-      setVerData([]);
-      data.forEach((element: any,index: string | number) => {
-        var type = data[index]["Version"];
-        var value = data[index]["Value"];
-        var dataPoint: Datapoint = {Parameter:type,Value:value}
-        list.push(dataPoint);
-      });
-      setVerData(list)
-      }}></DataSelector>
-    </Container>
-    {verData && <Container sx={{bgcolor:'background.paper', p:3}}>
-      <DataTable document={verData} ></DataTable>
-    </Container>}
-    <Container sx={{bgcolor:'background.paper', p:3}}>
-      {text}
-    </Container>
-    {/* <Grid container  sx={{p:2, height:'30vh',bgcolor:'background.paper'}}>
+      <Container sx={{ height: '30vh', bgcolor: 'background.paper' }}>
+        <DataSelector onGotData={(data) => {
+          if (!data) return;
+          setText(JSON.stringify(data, null, 2));
+          var list: Datapoint[] = [];
+          setVerData([]);
+          data.forEach((element: any, index: string | number) => {
+            var type = data[index]["Version"];
+            var value = data[index]["Value"];
+            var dataPoint: Datapoint = { Parameter: type, Value: value }
+            list.push(dataPoint);
+          });
+          setVerData(list)
+        }}></DataSelector>
+      </Container>
+      {verData && <Container sx={{ bgcolor: 'background.paper', p: 3 }}>
+        <DataTable document={verData} ></DataTable>
+      </Container>}
+      <Container sx={{ bgcolor: 'background.paper', p: 3 }}>
+        {text}
+      </Container>
+      {/* <Grid container  sx={{p:2, height:'30vh',bgcolor:'background.paper'}}>
     </Grid> */}
     </>
   )
--- frontend/src/components/MetaDataDisplay.tsx
+++ frontend/src/components/MetaDataDisplay.tsx
@@ -21,24 +21,27 @@ const MetaTable: React.FC<Props> = ({ document, title }) => {
   };
 
   return (
-    <div>
-      <Typography variant='h4' align='center'>{title}</Typography>
-      <TableContainer component={Paper} sx={{ maxHeight: 150, overflow: 'auto' }}> {/* Set maxHeight and overflow */}
-        <Table>
-          <TableHead>
-          </TableHead>
-          <TableBody>
-            {metaDataKeys.map((key: string, index: number) => (
-              <TableRow key={index}>
-                <TableCell>{key}</TableCell>
-                <TableCell>{renderValue(document[0][key])}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <Typography variant='h4' align='center' style={{ flex: '0 0 auto' }}>{title}</Typography>
+      <div style={{ flex: '1 1 auto', overflow: 'auto' }}>
+        <Paper>
+          <TableContainer>
+            <Table>
+              <TableBody>
+                {metaDataKeys.map((key: string, index: number) => (
+                  <TableRow key={index}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{renderValue(document[0][key])}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </div>
     </div>
   );
 };
 
 export default MetaTable;
+
